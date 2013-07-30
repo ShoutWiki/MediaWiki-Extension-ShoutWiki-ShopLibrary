dcf6659227d0804fffbe9861902f06425d0d58cb
lcawte <lewis@lewiscawte.info>
2013-07-30 16:04:41 +0100

    Push basis of ShopLibrary.

---
+++ ShopLibrary.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Extensions
+ * @author Lewis Cawte
+ * @copyright Copyright © 2013, Lewis Cawte
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+	die();
+}
+
+$wgExtensionCredits['specialpage'][] = array(
+	'path'           => __FILE__,
+	'name'           => 'ShoutWiki Shop Library',
+	'author'         => 'Lewis Cawte',
+	'descriptionmsg' => 'shoplibrary-desc',
+	'url'            => 'https://www.mediawiki.org/wiki/Extension:ShoutWiki_Shop_Library',
+	'version'		 => '0.1',
+);
+
+$dir = dirname(__FILE__) . '/';
+$wgAutoloadClasses['ShopLibraryHooks'] = $dir . 'ShopLibrary.hooks.php';
+$wgExtensionMessagesFiles['ShopLibrary'] = $dir . 'ShopLibrary.i18n.php';

968488e7a595ae548c7316fa7be84c9e4c1a85bc
Lewis Cawte <lewis@lewiscawte.me>
2013-07-30 17:29:08 +0100

    Finish header links in ShopLibrary.hooks.php. Add ShopLibrary.i18n.php for i18n messages.

--- ShopLibrary.hooks.php
+++ ShopLibrary.hooks.php
@@ -3,6 +3,17 @@
 class ShopLibraryHooks {
 	
 	public function buildSWShopHeader() {
+		wfRunHooks( 'ShoutWikiShopHeaderItems', $links );
+		//similar crap from Linker.php line 1164 to format
+		
+		if ( $links ) {
+			return wfMessage( 'word-separator' )->plain()
+			. '<span class="sw-shopheaderlinks">'
+			. wfMessage( 'parentheses' )->rawParams( $wgLang->pipeList( $links ) )->escaped()
+			. '</span>';
+		} else {
+			return '';
+		}
 		
 	}
 	
---
+++ ShopLibrary.i18n.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Internationalisation file for extension ShopLibrary.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English (English)
+ * @author Lcawte
+ */
+$messages['en'] = array(
+		'shoplibrary-desc'     => 'Adds a common library for ShoutWiki Shop Extensions.',
+);
